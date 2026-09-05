ab0342d3ed22455572317d0b43c86e26b1e105bf
Konstantin Klimov <klimov3@yandex.ru>
2026-09-05 22:44:46 +0300

    Initial Commit

---
+++ Road Master/Road_MasterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Road_MasterApp.swift
+//  Road Master
+//
+//  Created by Константин Климов on 05.09.2026.
+//
+
+import SwiftUI
+
+@main
+struct Road_MasterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
